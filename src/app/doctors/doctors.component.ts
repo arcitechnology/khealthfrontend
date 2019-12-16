1341531f0b23a715c9083d66acda2fa08f6fa34f
unknown <arcitechnologies@gmail.com>
2019-12-16 22:58:15 +0530

    pagination set to departments_doctors_patients

--- src/app/doctors/doctors.component.ts
+++ src/app/doctors/doctors.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DoctorsService } from '../services/doctors.service';
 import { Idoctor } from '../prototypes/doctorprototype';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-doctors',
@@ -8,12 +9,26 @@ import { Idoctor } from '../prototypes/doctorprototype';
   styleUrls: ['./doctors.component.css'],
   providers:[DoctorsService]
 })
-export class DoctorsComponent implements OnInit {
+export class DoctorsComponent implements OnInit, OnDestroy {
 
   doctors:Idoctor[];
-  constructor(private _doctorService: DoctorsService) { 
-    this._doctorService.getDoctors().subscribe((data) => { this.doctors = data; });
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
+  
+  constructor(private _doctorService: DoctorsService) {
+	this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10
+    };
   }
   ngOnInit() {
+	this._doctorService.getDoctors().subscribe((data) => { 
+      this.doctors = data;
+	  this.dtTrigger.next();    
+    });
+  }
+  
+  ngOnDestroy(){
+	this.dtTrigger.unsubscribe();
   }
 }
--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PatientsService } from '../services/patients.service';
 import { Ipatient } from '../prototypes/patientprototype';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-patients',
@@ -8,14 +9,27 @@ import { Ipatient } from '../prototypes/patientprototype';
   styleUrls: ['./patients.component.css'],
   providers:[PatientsService]
 })
-export class PatientsComponent implements OnInit {
+export class PatientsComponent implements OnInit, OnDestroy {
 
   patients:Ipatient[];
+  dtOptions:DataTables.Settings = {};
+  dtTrigger:Subject<any> =  new Subject();
   constructor(private _patientsService:PatientsService) { 
-    this._patientsService.getPatients().subscribe((data) => { this.patients = data; });
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10
+    };
   }
 
   ngOnInit() {
+	this._patientsService.getPatients().subscribe((data) => {
+		this.patients = data;
+		this.dtTrigger.next();
+	});
+  }
+  
+  ngOnDestroy(){
+	this.dtTrigger.unsubscribe();
   }
 
 }
