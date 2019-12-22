6ae4ae08e4cf799cbea661cee48392a97ec8176d
unknown <arcitechnologies@gmail.com>
2019-12-22 14:16:57 +0530

    removed fileupload code

--- src/app/guards/can-deactivate.gaurd.ts
+++ src/app/guards/can-deactivate.gaurd.ts
@@ -1,4 +1,3 @@
-import { FileUploadComponent } from '../modules/uploads/file-upload/file-upload.component';
 import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -6,7 +5,7 @@ export class CanDeactivateUploadRedirect implements CanDeactivate<FileUploadComp
     constructor() {
     }
 
-    canDeactivate(target: FileUploadComponent, currentRoute: ActivatedRouteSnapshot,
+    canDeactivate(currentRoute: ActivatedRouteSnapshot,
         currentState: RouterStateSnapshot,
         nextState: RouterStateSnapshot): Promise<boolean> {
         return target.hasChanges();
