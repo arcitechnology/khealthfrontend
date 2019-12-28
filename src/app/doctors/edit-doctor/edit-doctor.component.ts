693509dd34cc73c6c661fdab79ea049752c279f2
unknown <arcitechnologies@gmail.com>
2019-12-28 22:45:15 +0530

    menubar issue 90 percent fixed

--- src/app/doctors/edit-doctor/edit-doctor.component.ts
+++ src/app/doctors/edit-doctor/edit-doctor.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DoctorsService } from '../../services/doctors.service';
+import { DepartmentsService } from '../../services/departments.service';
+import { Idoctor } from '../../prototypes/doctorprototype';
+import { Idepartment } from '../../prototypes/departmentprototype';
+import {Router} from '@angular/router';
+import Swal, { SweetAlertOptions } from 'sweetalert2';
+import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
 
 @Component({
   selector: 'app-edit-doctor',
@@ -7,9 +15,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditDoctorComponent implements OnInit {
 
-  constructor() { }
+  doctor:Idoctor;
+  departments:any;
+  selectedDepArray: Array<any> = [];
+  constructor(private _doctorService:DoctorsService, private router:Router, public sweetAlertService: SweetAlertService, private _departments:DepartmentsService) { 
+	this._departments.getDepartments().subscribe((data) => { this.departments = data; });
+  }
 
   ngOnInit() {
+  }
+  onSubmit(doctor:Idoctor){
+		const payload = { doctor, departments: this.selectedDepArray };
+		console.log(payload);
+		this._doctorService.saveDoctor(payload).subscribe(
+		  (data) => {
+			this.sweetAlertService.showAlert('success', 'Doctor registered successfully.', 'Done!');
+			this.router.navigate(['/doctors']);
+		  },
+		  (error:any) => {
+			console.log(error)
+		  }
+		);
+  }
+  onChange(dep: string, isChecked: boolean) {
+    if (isChecked) {
+      this.selectedDepArray.push(dep);
+    } else {
+      let index = this.selectedDepArray.indexOf(dep);
+      this.selectedDepArray.splice(index, 1);
+    }
   }
 
 }
--- src/app/menubar/menubar.component.ts
+++ src/app/menubar/menubar.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
 import { Router } from '@angular/router';
-
+import 'metismenu';
+declare let $: any;
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
   styleUrls: ['./menubar.component.css']
 })
-export class MenubarComponent implements OnInit {
+export class MenubarComponent implements OnInit, AfterViewInit {
 
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+	$('.button-menu-mobile').on('click',function(event){
+		event.preventDefault();
+		$('body').toggleClass("enlarged");
+	});
   }
+  ngAfterViewInit() {
+      $('#side-menu').metisMenu();
+   }
 
   logout() {
     this.authService.logout().subscribe(res => {
